8b857680008d59bee2ebc37781cdd6e6fdfa20ad
Anand Shankar <anand.shankar.r@outlook.com>
2023-03-13 19:00:09 -0400

    20230313

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import numpy as np
 import datetime as dt
 import pickle
 import os
-from flask import Flask, render_template,make_response
+from flask import Flask, render_template,request
 
-
+# https://www.geeksforgeeks.org/how-to-use-web-forms-in-a-flask-application/
 # Initalize Flask
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-rfcl_model_url = os.path.join(SITE_ROOT, "static/models", "rfcl.pkl")
+lgcl_model_url = os.path.join(SITE_ROOT, "static/models", "lgcl.pkl")
+kncl_model_url = os.path.join(SITE_ROOT, "static/models", "kncl.pkl")
+svcl_model_url = os.path.join(SITE_ROOT, "static/models", "svcl.pkl")
 dtcl_model_url = os.path.join(SITE_ROOT, "static/models", "dtcl.pkl")
+rfcl_model_url = os.path.join(SITE_ROOT, "static/models", "rfcl.pkl")
 
-rfmodel = pickle.load(open(rfcl_model_url, 'rb'))
+# lgmodel = pickle.load(open(lgcl_model_url, 'rb'))
+# knmodel = pickle.load(open(kncl_model_url, 'rb'))
+# svmodel = pickle.load(open(svcl_model_url, 'rb'))
 # dtmodel = pickle.load(open(dtcl_model_url,'rb'))
+rfmodel = pickle.load(open(rfcl_model_url, 'rb'))
 
 # prediction function
 def ValuePredictor(to_predict_list):
@@ -25,18 +32,25 @@ def ValuePredictor(to_predict_list):
 def home():
     return render_template('index.html')
  
-@app.route('/result', methods = ['POST'])
+@app.route('/heart', methods = ['POST'])
 def result():
     if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list)       
-        if int(result)== 1:
-            prediction ='Probable Heart Disease '
-        else:
-            prediction ='Heart Disease improbable'
-        return render_template("index.html", prediction = prediction)
+        init_features = [float(x) for x in request.form.values()]
+        # xto_predict_list = [np.array(init_features)]
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5009)
+        # to_predict_list = request.form.to_dict()
+        # to_predict_list = list(to_predict_list.values())
+        # to_predict_list = list(map(int, to_predict_list))
+        # result = ValuePredictor(to_predict_list)       
+        # if int(result)== 1:
+            #  prediction ='Probable Heart Disease '
+        # else:
+            #  prediction ='Heart Disease improbable'
+
+        value_list = {"1"}
+        prediction = {"xxxxx"}
+        return render_template("index.html", values_list = value_list, prediction = prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
